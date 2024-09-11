0950c4b819f0cbceab6890b4a8e9df00dd92023d
RANJAN PALAI <palairanjan2002@gmail.com>
2024-09-12 00:01:51 +0530

    folder structure added

---
+++ Celebrity-Manager/src/components/CelebrityAccordion.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CelebrityAccordion = () => {
+  return (
+    <div>CelebrityAccordion</div>
+  )
+}
+
+export default CelebrityAccordion
--- Celebrity-Manager/src/main.tsx
+++ Celebrity-Manager/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { StyledEngineProvider } from "@mui/material/styles";
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <StyledEngineProvider injectFirst>
     <App />
-  </StrictMode>,
+  </StyledEngineProvider>,
 )
